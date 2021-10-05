cfb109f50a1a670626aa4229eb166f80c5ec80b7
sg892-admin <zain@s4smartphones.com>
2021-10-05 13:52:45 +0500

    First Commit0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+//imports
+import React, {useState, useEffect} from 'react'
+import {ResponsiveContainer, BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar} from 'recharts'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const App = () => {
+  const url = 'https://api.coincap.io/v2/assets/?limit=10'
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    fetch(url).then((response) => {
+      if (response.ok) { //response.ok return tru if response is succesfully returned.
+       return response.json()
+      }
+    })
+    .then((data) => {
+      setData(data.data) //set data coming from fetch api to state.
+    })
+    .catch((error) => {
+      console.log("Error: ", error) 
+    })
+  }, [])
+
+return (
+  <React.Fragment>
+  <ResponsiveContainer width="100%" aspect={2}>
+        <BarChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+          top: 50,
+          right: 30,
+          left: 20,
+          bottom: 5,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis dataKey="priceUsd" domain={[0, 50000]} tickCount={8}/> {/* Customized Bar*/}
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="priceUsd" fill="#8884d8" />
+          <Bar dataKey="names" fill="#82ca9d" />
+        </BarChart>
+      </ResponsiveContainer>
+    </React.Fragment>
+)}
 export default App;
